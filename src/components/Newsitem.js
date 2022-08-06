3ecbdfc4cca4650e38cc44105d9b541d75b5575e
Anukarsh <anukarshr@gmail.com>
2022-08-06 22:26:57 +0530

    update

--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -2,19 +2,17 @@ import { getByTitle } from "@testing-library/react";
 import React, { Component } from "react";
 
 export class Newsitem extends Component {
-
+ 
   render() {
-    let {title, description, imageUrl} = this.props
+    let { title, description, imageUrl, newsUrl } = this.props;
     return (
-      <div className="card" style={{width: "18rem"}}>
+      <div className="card my-3" style={{ width: "18rem" }}>
         <img src={imageUrl} className="card-img-top" alt="..." />
         <div className="card-body">
           <h5 className="card-title">{title}</h5>
-          <p className="card-text">
-            {description}
-          </p>
+          <p className="card-text">{description}</p>
           <a href="#" className="btn btn-primary">
-            Go somewhere
+            Read More
           </a>
         </div>
       </div>
