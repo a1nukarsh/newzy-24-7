8d09bba0719a2007c1d3c118e177377aa5e7d8c7
Anukarsh <anukarshr@gmail.com>
2022-08-13 14:41:25 +0530

    update

--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -6,7 +6,7 @@ export class Newsitem extends Component {
   render() {
     let { title, description, imageUrl, newsUrl } = this.props;
     return (
-      <div className="card my-3" style={{ width: "18rem" }}>
+      <div className="card my-3">
         <img src={imageUrl?imageUrl:"https://images.wsj.net/im-599395/social"} className="card-img-top" alt="..." />
         <div className="card-body">
           <h5 className="card-title">{title}...</h5>
