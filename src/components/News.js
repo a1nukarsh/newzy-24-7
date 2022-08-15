bf8e4dbe52138b4a1017a6da3b7f1bd15e2d8f3c
Anukarsh <anukarshr@gmail.com>
2022-08-15 13:54:53 +0530

    Update News.js
    Converting Class Based News component(body that contains news cards) to Function Based

--- src/components/News.js
+++ src/components/News.js
@@ -1,195 +1,197 @@
-import React, { Component } from 'react'
-import Newsitem from './Newsitem'
-import Spinner from './Spinner';
-import PropTypes from 'prop-types'
+import React, { useEffect, useState } from "react";
+import Newsitem from "./Newsitem";
+import Spinner from "./Spinner";
+import PropTypes from "prop-types";
 import InfiniteScroll from "react-infinite-scroll-component";
 
-export class News extends Component {
-    static defaultProps = {
-        country: 'in',
-        pageSize: 8,
-        category: 'general',
-    }
-    static propTypes = {
-        name: PropTypes.string,
-        pageSize: PropTypes.number,
-        category: PropTypes.string,
-    }
+const News = (props) => {
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [totalResults, setTotalResults] = useState(0);
+  // document.title = `${this.capitalizeFirstLetter(props.category)} - Newzy247`
 
-    articles = [
-        {
-        "source": {
-        "id": "bbc-sport",
-        "name": "BBC Sport"
-        },
-        "author": "BBC Sport",
-        "title": "Shane Warne memorial - watch & follow updates",
-        "description": "Watch live coverage and follow text updates and tributes from the state memorial for Australian cricket legend Shane Warne at the Melbourne Cricket Ground.",
-        "url": "http://www.bbc.co.uk/sport/live/cricket/60916236",
-        "urlToImage": "https:////m.files.bbci.co.uk/modules/bbc-morph-sport-seo-meta/1.22.0/images/bbc-sport-logo.png",
-        "publishedAt": "2022-03-30T08:22:26.498888Z",
-        "content": "Former England bowler and BBC cricket presenter Isa Guha, who became a colleague of Warne's in the commentary box: \"It has been a strange few weeks - a lot of shock and then we did our own tribute at… [+396 chars]"
-        },
-        {
-        "source": {
-        "id": "espn-cric-info",
-        "name": "ESPN Cric Info"
-        },
-        "author": null,
-        "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-        "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-        "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-        "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-        "publishedAt": "2020-04-27T11:41:47Z",
-        "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-        },
-        {
-        "source": {
-        "id": "espn-cric-info",
-        "name": "ESPN Cric Info"
-        },
-        "author": null,
-        "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-        "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-        "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-        "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-        "publishedAt": "2020-03-30T15:26:05Z",
-        "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-        }
-        ]
+  // articles = [
+  //     {
+  //     "source": {
+  //     "id": "bbc-sport",
+  //     "name": "BBC Sport"
+  //     },
+  //     "author": "BBC Sport",
+  //     "title": "Shane Warne memorial - watch & follow updates",
+  //     "description": "Watch live coverage and follow text updates and tributes from the state memorial for Australian cricket legend Shane Warne at the Melbourne Cricket Ground.",
+  //     "url": "http://www.bbc.co.uk/sport/live/cricket/60916236",
+  //     "urlToImage": "https:////m.files.bbci.co.uk/modules/bbc-morph-sport-seo-meta/1.22.0/images/bbc-sport-logo.png",
+  //     "publishedAt": "2022-03-30T08:22:26.498888Z",
+  //     "content": "Former England bowler and BBC cricket presenter Isa Guha, who became a colleague of Warne's in the commentary box: \"It has been a strange few weeks - a lot of shock and then we did our own tribute at… [+396 chars]"
+  //     },
+  //     {
+  //     "source": {
+  //     "id": "espn-cric-info",
+  //     "name": "ESPN Cric Info"
+  //     },
+  //     "author": null,
+  //     "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
+  //     "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
+  //     "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
+  //     "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
+  //     "publishedAt": "2020-04-27T11:41:47Z",
+  //     "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
+  //     },
+  //     {
+  //     "source": {
+  //     "id": "espn-cric-info",
+  //     "name": "ESPN Cric Info"
+  //     },
+  //     "author": null,
+  //     "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
+  //     "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
+  //     "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
+  //     "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
+  //     "publishedAt": "2020-03-30T15:26:05Z",
+  //     "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
+  //     }
+  //     ]
 
-    capitalizeFirstLetter = (string)=>{
-        return string.charAt(0).toUpperCase()+string.slice(1)
-    }
-    constructor(props) {
-        super(props);
-        console.log("Hello I am a Constructor");
-        this.state={
-            articles: this.articles,
-            loading: true,
-            page:1,
-            totalResults: 0
-        }
-        document.title = `${this.capitalizeFirstLetter(this.props.category)} - Newzy247`
-      }
+  const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
 
-      async updateNews(){
-        this.props.setProgress(10)
-        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`
-        this.setState({loading:true})
-        let data = await fetch(url)
-        this.props.setProgress(30)
-        let parsedData = await data.json()
-        this.props.setProgress(72)
-        console.log(parsedData)
-        this.setState({
-            articles: parsedData.articles, 
-            totalResults: parsedData.totalResults,
-            loading:false,
-        })
-        this.props.setProgress(100)
+  const updateNews = async () => {
+    props.setProgress(10);
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
+    // this.setState({ loading: true });
+    setLoading(true)
+    let data = await fetch(url);
+    props.setProgress(30);
+    let parsedData = await data.json();
+    props.setProgress(72);
+    console.log(parsedData);
+    setArticles(parsedData.articles);
+    setTotalResults(parsedData.totalResults);
+    setLoading(false);
+    // this.setState({
+    //     articles: parsedData.articles,
+    //     totalResults: parsedData.totalResults,
+    //     loading:false,
+    // })
+    props.setProgress(100);
+  };
 
-    }
+  useEffect(() => {
+    updateNews();
+  }, []);
 
-      async componentDidMount(){
-        // console.log('cdm')
-        // let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=6da6252ff0384d25b601d7388b662111&page=1&pageSize=${this.props.pageSize}`
-        // this.setState({loading:true})
-        // let data = await fetch(url)
-        // let parsedData = await data.json()
-        // console.log(parsedData)
-        // this.setState({
-        //     articles: parsedData.articles, 
-        //     totalResults: parsedData.totalResults,
-        //     loading:false
-        // })
-        this.updateNews()
-    }
-    
-    handlePrevClick = async ()=>{
-        // console.log("Prev is clicked")
-        // let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=6da6252ff0384d25b601d7388b662111&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`
-        // this.setState({loading:true})
-        //     let data = await fetch(url)
-        //     let parsedData = await data.json()
-        //     console.log(parsedData)
-        //     this.setState({
-        //         page: this.state.page - 1,
-        //         articles: parsedData.articles,
-        //         loading:false
+  const handlePrevClick = async () => {
+    // console.log("Prev is clicked")
+    // let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=6da6252ff0384d25b601d7388b662111&page=${this.state.page - 1}&pageSize=${props.pageSize}`
+    // this.setState({loading:true})
+    //     let data = await fetch(url)
+    //     let parsedData = await data.json()
+    //     console.log(parsedData)
+    //     this.setState({
+    //         page: this.state.page - 1,
+    //         articles: parsedData.articles,
+    //         loading:false
 
-        //     })
-        this.setState({page:this.state.page -1})
-        this.updateNews()
-    }
-    handleNextClick = async ()=>{
-        // console.log("nect is clicked")
-        // if(!(this.state.page + 1> Math.ceil(this.state.totalResults/this.props.pageSize))){
-            //     let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=6da6252ff0384d25b601d7388b662111&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`
-            //     this.setState({loading:true})
-            //     let data = await fetch(url)
-            //     let parsedData = await data.json()
-            //     this.setState({
-                //         page: this.state.page + 1,
-                //         articles: parsedData.articles,
-                //         loading:false
-                
-                //     })
-                // }
-                this.setState({page:this.state.page +1})
-                this.updateNews()
-            }
+    //     })
+    // this.setState({ page: this.state.page - 1 });
+    setPage(page - 1)
+    updateNews();
+  };
+  const handleNextClick = async () => {
+    // console.log("nect is clicked")
+    // if(!(this.state.page + 1> Math.ceil(this.state.totalResults/props.pageSize))){
+    //     let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=6da6252ff0384d25b601d7388b662111&page=${this.state.page + 1}&pageSize=${props.pageSize}`
+    //     this.setState({loading:true})
+    //     let data = await fetch(url)
+    //     let parsedData = await data.json()
+    //     this.setState({
+    //         page: this.state.page + 1,
+    //         articles: parsedData.articles,
+    //         loading:false
 
-            fetchMoreData = async () => {
-                this.setState({page: this.state.page + 1})
-                const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`
-                // this.setState({loading:true})
-                let data = await fetch(url)
-                let parsedData = await data.json()
-                console.log(parsedData)
-                this.setState({
-                    articles: this.state.articles.concat(parsedData.articles), 
-                    totalResults: parsedData.totalResults
-                    // loading:false,
-                })
-              };
-            
-  render() {
-      console.log('render')
-    return (
-        <>
-        <h1 className='text-center'>Newzy247 - {this.capitalizeFirstLetter(this.props.category)} News</h1>
-        {this.state.loading && <Spinner/>}
+    //     })
+    // }
+    // this.setState({ page: this.state.page + 1 });
+    setPage(page + 1)
+    updateNews();
+  };
 
-        <InfiniteScroll
-          dataLength={this.state.articles.length}
-          next={this.fetchMoreData}
-          hasMore={this.state.articles.length !== this.state.totalResults}
-          loader={this.state.loading && <Spinner/>}
-        >
-          {/* {this.state.items.map((i, index) => (
+  const fetchMoreData = async () => {
+    // this.setState({ page: this.state.page + 1 });
+    setPage(page + 1)
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
+    // this.setState({loading:true})
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    setArticles(articles.concat(parsedData.articles))
+    setTotalResults(parsedData.totalResults)
+    // this.setState({
+    //   articles: articles.concat(parsedData.articles),
+    //   totalResults: parsedData.totalResults,
+    //   // loading:false,
+    // });
+  };
+
+  return (
+    <>
+      <h1 className="text-center">
+        Newzy247 - {capitalizeFirstLetter(props.category)} News
+      </h1>
+      {loading && <Spinner />}
+
+      <InfiniteScroll
+        dataLength={articles.length}
+        next={fetchMoreData}
+        hasMore={articles.length !== totalResults}
+        loader={loading && <Spinner />}
+      >
+        {/* {this.state.items.map((i, index) => (
             <div style={style} key={index}>
               div - #{index}
             </div>
           ))} */}
         <div className="container">
-
-            <div className="row">
-            {this.state.articles.map((element)=>{
-                return <div className="col-md-3" key={element.url}>
-                    <Newsitem title={element.title?element.title:"blank"} description={element.description?element.description:"blank"} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name}/>
+          <div className="row">
+            {articles.map((element) => {
+              return (
+                <div className="col-md-3" key={element.url}>
+                  <Newsitem
+                    title={element.title ? element.title : "blank"}
+                    description={
+                      element.description ? element.description : "blank"
+                    }
+                    imageUrl={element.urlToImage}
+                    newsUrl={element.url}
+                    author={element.author}
+                    date={element.publishedAt}
+                    source={element.source.name}
+                  />
                 </div>
-                })}
-                </div>
-            </div> 
-        </InfiniteScroll>
-        {/* <div className="container d-flex justify-content-between my-5">
+              );
+            })}
+          </div>
+        </div>
+      </InfiniteScroll>
+      {/* <div className="container d-flex justify-content-between my-5">
         <button disabled={this.state.page<=1} type="button" className="btn btn-danger" onClick={this.handlePrevClick}> &larr; Previous</button>
-        <button disabled={this.state.page + 1> Math.ceil(this.state.totalResults/this.props.pageSize)} type="button" className="btn btn-success" onClick={this.handleNextClick}>Next &rarr;</button>
+        <button disabled={this.state.page + 1> Math.ceil(this.state.totalResults/props.pageSize)} type="button" className="btn btn-success" onClick={this.handleNextClick}>Next &rarr;</button>
         </div>    */}
-      </>
-    )
-  }
-}
+    </>
+  );
+};
 
-export default News
+News.defaultProps = {
+  country: "in",
+  pageSize: 8,
+  category: "general",
+};
+News.propTypes = {
+  name: PropTypes.string,
+  pageSize: PropTypes.number,
+  category: PropTypes.string,
+};
+
+export default News;
